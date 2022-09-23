9ef721706ac1289f18d9264f705ace61a44b13ea
root <root@ip-172-31-47-151.ap-south-1.compute.internal>
2022-09-23 04:18:38 +0000

    this is made changes to Pythom by santhu

--- pythom.py
+++ pythom.py
@@ -1,4 +1,12 @@
 sum=a+b
 sum1=a-b
 sum=a*b
+sum=" Hi Santhosh this is demo classum1=a-b
+sum=a*b
 sum=" Hi Santhosh this is demo class"
+sum1=a-b
+sum=a*b
+sum=" Hi keerthana this is demo class"
+sum1=a-b
+sum=a*b
+sum=" Hi Karthik this is demo class"s"
